33849b1ab2911eef0b9d041f96c846179c1b00c6
dmaniaa <danielmania93@gmail.com>
2023-05-18 19:56:06 +0200

    divided code into functions; added manual run path

--- main.py
+++ main.py
@@ -7,21 +7,31 @@ app = Flask(__name__)
 
 logs = []
 
-@app.route('/', methods=['POST'])
+def add_log(message):
+    logs.append({ 
+            'date': time.strftime('%X %x %Z'),
+            'message': message
+        })
+    
+def run_pmm():
+    runner = subprocess.run(["python", "plex_meta_manager.py", "-r"])
+    if (runner.returncode == 0):
+        add_log("PMM run finised successfully")
+        response = make_response("OK", 200)
+    else:
+        add_log("PMM run failed")
+        response = make_response("Internal Server Error", 500)
+    return response
+
+@app.route('/plex', methods=['POST'])
 def plex():
     data = request.form.to_dict()
     parsed = json.loads(data['payload'])
     if (parsed['event'] == 'library.new'):
-        logs.append({ 
-            'date': time.strftime('%X %x %Z'),
-            'message': 'Update webhook received'
-        })
-        subprocess.run(["python", "plex_meta_manager.py", "-r"])
-        logs.append({ 
-            'date': time.strftime('%X %x %Z'),
-            'message': 'PMM run succeeded'
-        })
-    response = make_response("OK", 200)
+        add_log("Plex webhook received")
+        response = run_pmm()
+    else:
+        response = make_response("No Action Taken", 200)
     return response
 
 @app.route('/logs', methods=['GET'])
@@ -32,6 +42,11 @@ def log():
             html += '<tr><td>' + entry['date'] + '</td><td>' + entry['message'] + '</td></tr>'
     html += '</table>'
     return make_response(html, 200)
+
+@app.route('/run', methods=['GET'])
+def run():
+    add_log("Manual run triggered")
+    return run_pmm()
 
 
 if __name__ == '__main__':
